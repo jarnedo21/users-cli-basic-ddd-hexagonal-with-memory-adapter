b117ed1a19b79470e25a5b083b59be3658a26531
jarnedo21 <jarnedoz@unicartagena.edu.co>
2026-09-19 04:11:13 -0500

    feat: integrar la gestion de clientes en el menu principal y en Main
    GuiCli agrega la opcion 4 para gestionar clientes y la opcion Salir pasa a ser la 5. Main ensambla manualmente los adaptadores, servicios y el controlador de clientes.

--- src/main/java/com/jcaa/udec/Main.java
+++ src/main/java/com/jcaa/udec/Main.java
@@ -1,14 +1,32 @@
 package com.jcaa.udec;
 
+import com.jcaa.udec.collections.adapter.persistence.memory.ActualizarClienteAdapter;
+import com.jcaa.udec.collections.adapter.persistence.memory.EliminarClienteAdapter;
+import com.jcaa.udec.collections.adapter.persistence.memory.GuardarClienteAdapter;
 import com.jcaa.udec.collections.adapter.persistence.memory.GuardarUsuarioAdapter;
+import com.jcaa.udec.collections.adapter.persistence.memory.ObtenerClientesAdapter;
 import com.jcaa.udec.collections.adapter.persistence.memory.ObtenerUsuariosAdapter;
+import com.jcaa.udec.collections.application.service.ActualizarClienteService;
+import com.jcaa.udec.collections.application.service.AgregarClienteService;
 import com.jcaa.udec.collections.application.service.AgregarUsuarioService;
+import com.jcaa.udec.collections.application.service.EliminarClienteService;
+import com.jcaa.udec.collections.application.service.ObtenerClientesService;
 import com.jcaa.udec.collections.application.service.ObtenerUsuariosService;
+import com.jcaa.udec.collections.application.service.ports.in.ActualizarClienteUseCase;
+import com.jcaa.udec.collections.application.service.ports.in.AgregarClienteUseCase;
 import com.jcaa.udec.collections.application.service.ports.in.AgregarUsuarioUseCase;
+import com.jcaa.udec.collections.application.service.ports.in.EliminarClienteUseCase;
+import com.jcaa.udec.collections.application.service.ports.in.ObtenerClienteUseCase;
 import com.jcaa.udec.collections.application.service.ports.in.ObtenerUsuarioUseCase;
+import com.jcaa.udec.collections.domain.port.out.ActualizarClientePort;
+import com.jcaa.udec.collections.domain.port.out.EliminarClientePort;
+import com.jcaa.udec.collections.domain.port.out.GuardarClientePort;
 import com.jcaa.udec.collections.domain.port.out.GuardarUsuarioPort;
+import com.jcaa.udec.collections.domain.port.out.ObtenerClientesPort;
 import com.jcaa.udec.collections.domain.port.out.ObtenerUsuariosPort;
 import com.jcaa.udec.collections.entrypoint.cli.GuiCli;
+import com.jcaa.udec.collections.entrypoint.controller.ClienteControlador;
+import com.jcaa.udec.collections.entrypoint.controller.ClienteControladorImpl;
 import com.jcaa.udec.collections.entrypoint.controller.UsuarioControlador;
 import com.jcaa.udec.collections.entrypoint.controller.UsuarioControladorImpl;
 
@@ -20,7 +38,23 @@ public class Main {
         ObtenerUsuarioUseCase obtenerUsuarioUseCase = new ObtenerUsuariosService(obtenerUsuariosPort);
         UsuarioControlador usuarioControlador =
                 new UsuarioControladorImpl(agregarUsuarioUseCase, obtenerUsuarioUseCase);
-        GuiCli guiCli = new GuiCli(usuarioControlador);
+
+        GuardarClientePort guardarClientePort = new GuardarClienteAdapter();
+        ObtenerClientesPort obtenerClientesPort = new ObtenerClientesAdapter();
+        ActualizarClientePort actualizarClientePort = new ActualizarClienteAdapter();
+        EliminarClientePort eliminarClientePort = new EliminarClienteAdapter();
+        AgregarClienteUseCase agregarClienteUseCase = new AgregarClienteService(guardarClientePort);
+        ObtenerClienteUseCase obtenerClienteUseCase = new ObtenerClientesService(obtenerClientesPort);
+        ActualizarClienteUseCase actualizarClienteUseCase =
+                new ActualizarClienteService(actualizarClientePort);
+        EliminarClienteUseCase eliminarClienteUseCase = new EliminarClienteService(eliminarClientePort);
+        ClienteControlador clienteControlador = new ClienteControladorImpl(
+                agregarClienteUseCase,
+                obtenerClienteUseCase,
+                actualizarClienteUseCase,
+                eliminarClienteUseCase);
+
+        GuiCli guiCli = new GuiCli(usuarioControlador, clienteControlador);
         guiCli.ejecutarAccion();
     }
 }
