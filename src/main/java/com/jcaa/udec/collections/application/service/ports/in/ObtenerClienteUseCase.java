a26de3c926bbd1d9d6564e191c410a64c119c995
jarnedo21 <jarnedoz@unicartagena.edu.co>
2026-09-19 03:38:29 -0500

    feat: agregar caso de uso para listar todos los clientes
    ObtenerClientesPort y ObtenerClienteUseCase exponen obtenerTodos(); el adaptador en memoria devuelve una copia inmutable de los clientes registrados.

--- src/main/java/com/jcaa/udec/collections/application/service/ports/in/ObtenerClienteUseCase.java
+++ src/main/java/com/jcaa/udec/collections/application/service/ports/in/ObtenerClienteUseCase.java
@@ -3,6 +3,10 @@ package com.jcaa.udec.collections.application.service.ports.in;
 import com.jcaa.udec.collections.application.service.dto.query.ObtenerClienteConsulta;
 import com.jcaa.udec.collections.domain.core.model.Cliente;
 
+import java.util.List;
+
 public interface ObtenerClienteUseCase {
+  List<Cliente> obtenerTodos();
+
   Cliente obtenerPorCodigo(ObtenerClienteConsulta consulta);
 }
--- src/main/java/com/jcaa/udec/collections/domain/port/out/ObtenerClientesPort.java
+++ src/main/java/com/jcaa/udec/collections/domain/port/out/ObtenerClientesPort.java
@@ -2,6 +2,10 @@ package com.jcaa.udec.collections.domain.port.out;
 
 import com.jcaa.udec.collections.domain.core.model.Cliente;
 
+import java.util.List;
+
 public interface ObtenerClientesPort {
+    List<Cliente> obtenerTodos();
+
     Cliente buscarPorCodigo(String codigo);
 }
